cef97167520b2153b5e052013147229b1e869372
Nishith Savla <63643696+Nishith-Savla@users.noreply.github.com>
2021-04-13 18:53:30 +0530

    Correct spaces before inline comment

--- Hackerrank/hackerrank_logo.py
+++ Hackerrank/hackerrank_logo.py
@@ -1,6 +1,6 @@
 #Replace all ______ with rjust, ljust or center. 
 
-thickness = int(input()) #This must be an odd number
+thickness = int(input())  # This must be an odd number
 c = 'H'
 
 #Top Cone
@@ -21,4 +21,4 @@ for i in range(thickness+1):
 
 #Bottom Cone
 for i in range(thickness):
-    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))jjjjjjjj
